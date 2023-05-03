0425c077c3b8d6368ea7ce7970117476202aba4d
wonderenlee <wonderenlee@gmail.com>
2023-05-03 16:44:28 -0400

    added double, sort, filter, and calculate

--- script.js
+++ script.js
@@ -26,7 +26,39 @@ async function getRandomUser() {
     addData(newUser);
 }
 
-// Add new object to data arr
+// Double everyone's money
+function doubleMoney() {
+    data = data.map(user => {
+        return { ...user, money: user.money * 2 };
+    });
+
+    updateDOM();
+}
+
+// Sort users by richest
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money);
+
+    updateDOM();
+}
+     
+// Filter only millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
+// Calculate the total wealth of user
+function calculateWealth() {
+    const wealth = data.reduce((accu, user) => (accu += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML =   `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`
+    main.appendChild(wealthEl);
+}
+
+// Add new object to data array
 function addData(obj) {
     data.push(obj);
 
@@ -48,5 +80,12 @@ function updateDOM(providedData = data) {
 
 // FOrmat number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
 function formatMoney(number) {
-    return '$' + number.toFixed(2).replaced(/\d(?=(\d{3})+\.)/g, '$&,');
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
+
+// Event listeners
+addUserBtn.addEventListener('click',getRandomUser);
+doubleBtn.addEventListener('click',doubleMoney);
+sortBtn.addEventListener('click',sortByRichest);
+showMillionairesBtn.addEventListener('click',showMillionaires);
+calculateWealthBtn.addEventListener('click',calculateWealth);
